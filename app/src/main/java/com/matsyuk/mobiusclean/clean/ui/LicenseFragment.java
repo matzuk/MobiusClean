d4f949add29e23d69a24d18aff6802c227115c7b
Eugene Matsyuk <matzuk2@mail.ru>
2017-04-02 13:51:27 +0300

    first views

---
+++ app/src/main/java/com/matsyuk/mobiusclean/clean/ui/LicenseFragment.java
@@ -0,0 +1,23 @@
+package com.matsyuk.mobiusclean.clean.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.matsyuk.mobiusclean.R;
+
+/**
+ * @author e.matsyuk
+ */
+public class LicenseFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fmt_license, container, false);
+    }
+
+}
